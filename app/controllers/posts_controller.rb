df9256e291af0aea2fcd4a14f6f23df8ef0352af
Alex Rehsitd <qblake.public@gmail.com>
2013-08-06 12:57:23 +0000

    add create, show, index actions to PostController. add views: index and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,22 @@ class PostsController < ApplicationController
   def new
   end
   def create
-    render text: params[:post].inspect
+    # render text: params[:post].inspect
+    @post = Post.new(params[:post].permit(:title, :text))
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end

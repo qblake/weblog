d60fe46f7e53b90b9970099fe3b90dec88028935
Alex Rehsitd <qblake.public@gmail.com>
2013-08-06 13:50:37 +0000

    add title validation. add edit and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
   def create
     # render text: params[:post].inspect
     @post = Post.new(params[:post].permit(:title, :text))
-    @post.save
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -14,6 +18,19 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   private

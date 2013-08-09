568b9d203eb697b805e38676ef69229de5341843
Alex Rehsitd <qblake.public@gmail.com>
2013-08-09 11:34:01 +0000

    add test for create, show, update and destroy actions

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   setup do
-    # @post = posts(:two)
+    @post = posts(:two)
     @request.env['HTTP_AUTHORIZATION'] =
       ActionController::HttpAuthentication::
       Basic.encode_credentials("dhhh", "secret")
@@ -17,5 +17,31 @@ class PostsControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should create post" do
+    assert_difference('Post.count') do
+      post :create, post: {title: 'Some title'}
+    end
+
+    assert_redirected_to post_path(assigns(:post))
+  end
+
+  test "should show post" do
+    get :show, id: @post.id
+    assert_response :success
+  end
+
+  test "should update post" do
+    patch :update, id: @post.id, post: {}
+    assert_redirected_to post_path(assigns(:post))
+  end
+
+  test "should destroy post" do
+    assert_difference('Post.count', -1) do
+      delete :destroy, id: @post.id
+    end
+
+    assert_redirected_to posts_path
   end
 end

40aee73bb2520670aed89859b878e327f43f6250
Alex Rehsitd <qblake.public@gmail.com>
2013-08-09 08:14:16 +0000

    add test for index and new actions

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    # @post = posts(:two)
+    @request.env['HTTP_AUTHORIZATION'] =
+      ActionController::HttpAuthentication::
+      Basic.encode_credentials("dhhh", "secret")
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:posts)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 end
